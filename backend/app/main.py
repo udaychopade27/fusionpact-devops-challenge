264356b086d312c0dbfbbffa50018358fcfcd5c1
Uday Chopade <udaychopade27@gmail.com>
2025-10-11 05:55:21 +0000

    dockerised backend and test app locally

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,3 +36,7 @@ async def get_users():
     Read user data from json file
     """
     return services.read_usersdata()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
